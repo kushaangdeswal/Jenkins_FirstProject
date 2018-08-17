1c1d479442fbf856edcba5e04f61e3f27cf49ae3
Kushaang Deswal <kushaangdeswal@outlook.com>
2018-08-17 11:22:57 +0530

    second iteration

--- src/ThirdIteration_UsingMars.java
+++ src/ThirdIteration_UsingMars.java
@@ -1,10 +1,12 @@
 import java. io.*;
+public class ThirdIteration_UsingMars
+{
 public static void main(String args[])
 {
 int a,b,c;
 a=10;
 b=20;
 c=30;
-System. out. println("output is" );
-return (a+b+c);
-}
+System. out. println("output is" + (a+b+c) );
+
+}}
